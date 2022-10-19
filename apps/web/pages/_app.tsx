2c3a97c827de66dc38279c193c2373c477e19e85
joaotuliojt <joaotuliosoares@hotmail.com>
2022-10-19 20:42:16 -0300

    feat: add context to make cards editable

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { AppProps } from "next/app";
 import "../styles/global.css";
 import { LayoutGroup } from "framer-motion";
+import { EditableModeProvider } from "../contexts/EditableMode";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const C = Component as React.ElementType;
 
   return (
-    <LayoutGroup>
+    <EditableModeProvider>
       <C {...pageProps} />
-    </LayoutGroup>
+    </EditableModeProvider>
   );
 };
 
