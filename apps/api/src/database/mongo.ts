71a48516a0a59ebb3fd5527d04ba5065213c0ef8
joaotuliojt <joaotuliosoares@hotmail.com>
2022-10-23 12:37:47 -0300

    feat: configure mongo memory server

--- apps/api/src/database/mongo.ts
+++ apps/api/src/database/mongo.ts
@@ -1,10 +1,32 @@
 import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server-global'
+import { config } from '../config';
+
+let mongod: MongoMemoryServer | null = null;
 
 export async function connectMongoDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string)
-    console.log("Mongo successfully connected!")
+    let dbUrl = config.MONGO_URI;
+    if (process.env.NODE_ENV === "test") {
+      mongod = await MongoMemoryServer.create();
+      dbUrl = mongod.getUri();
+    }
+    const conn = await mongoose.connect(dbUrl)
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error: any) {
-    throw new Error(error)
+    console.log(error);
+    process.exit(1)
+  }
+}
+
+export async function disconnectDB() {
+  try {
+    await mongoose.connection.close();
+    if (mongod) {
+      await mongod.stop()
+    }
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
   }
 }
