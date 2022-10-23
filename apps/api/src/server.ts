f15d4f9bb5595d161120ff2b7e6c57feed78b4a5
joaotuliojt <joaotuliosoares@hotmail.com>
2022-10-23 17:19:47 -0300

    feat(api): add cors

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import { graphqlHTTP } from "express-graphql"
 import expressPlayground from 'graphql-playground-middleware-express';
 import { schema } from './graphql/schema';
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
